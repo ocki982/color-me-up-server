5b9ca7fa02489cca469065fe80f9eb1fe0469c1d
ocki982 <maiderocta@gmail.com>
2022-05-18 23:38:14 -0700

    added auth token when user login

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router(); 
 const User = require("../models/Users");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 
+// Register
 router.post("/register", (req, res) => {
 try{
     const { username, email, password } = req.body;
@@ -12,7 +14,7 @@ try{
         return res.status(400).send("Please enter the required fields.");
     }
 
-    const hashedPassword = bcrypt.hashSync(password, 12);
+    const hashedPassword = bcrypt.hashSync(password, 10);
 
     const newUser = new User({
         username: username,
@@ -26,5 +28,32 @@ try{
     res.status(500).json(err)
   }
 });
+
+// Login
+router.post("/login", async (req, res) => {
+ 
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword =  await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400).json("wrong password");
+
+    if (user) {
+      const token = jwt.sign(
+        {
+          username: user.username,
+          email: user.email,
+        },
+        process.env.JWT_KEY,
+        { expiresIn: "24h" }
+      );
+
+      return res.json({ status: "ok", user: token})
+    } else {
+      return res.json({ status: "error", user: false})
+    }
+
+});
+
 
 module.exports = router;
