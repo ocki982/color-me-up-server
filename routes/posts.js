ee89c5fd57416cbc1e38cad917e93fa79bc3d05f
ocki982 <maiderocta@gmail.com>
2022-05-19 20:05:07 -0700

    post request to make new posts on the page

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router(); 
+const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-    console.log("post page")
-})
+router.post("/", (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+        newPost.save();
+        res.status(200).send("Posted!");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
