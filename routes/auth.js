9404e4a8d83fa3a98dc21fd122751b6b72d71517
ocki982 <maiderocta@gmail.com>
2022-05-19 19:12:27 -0700

    created authenticate middleware to get user based on id

--- routes/auth.js
+++ routes/auth.js
@@ -23,20 +23,20 @@ try{
     });
 
     newUser.save();
-    res.status(200).json(newUser);
+    res.status(200).send("Registered successfully");
   } catch (err) {
-    res.status(500).json(err)
-  }
+    res.status(500).send("Failed registration");
+  };
 });
 
 // Login
 router.post("/login", async (req, res) => {
  
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(404).json("user not found");
+    if(!user) { return res.status(404).send("User not found"); }
 
     const validPassword =  await bcrypt.compare(req.body.password, user.password)
-    !validPassword && res.status(400).json("wrong password");
+    if(!validPassword) { return res.status(400).send("Invalid password"); }
 
     if (user) {
       const token = jwt.sign(
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router(); 
+const authenticate = require('../middleware/authenticate');
+const User = require("../models/Users");
 
-router.get("/", (req, res) => {
-    res.send("Welcome to users")
-  })
+
+// get a user
+
+router.get('/:id', authenticate, async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+});
 
 module.exports = router;
