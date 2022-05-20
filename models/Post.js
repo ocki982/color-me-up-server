03874e3d466502c72da3c60a66a6b94b08599e09
ocki982 <maiderocta@gmail.com>
2022-05-19 19:45:31 -0700

    created delete user call on api

---
+++ models/Post.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        require: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+        max: 500,
+      },
+      emotion:{
+          type: String,
+      },
+      likes: {
+        type: Array,
+        default: [],
+      },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
--- routes/users.js
+++ routes/users.js
@@ -6,14 +6,26 @@ const User = require("../models/Users");
 
 // get a user
 
-router.get('/:id', authenticate, async(req, res) => {
+router.get('/current', authenticate, async(req, res) => {
     try {
-        const user = await User.findById(req.params.id);
+        const user = await User.findOne({ email: req.user.email });
+        // filters password and unrelated data on response
         const { password, updatedAt, ...other } = user._doc;
         res.status(200).json(other);
       } catch (err) {
         res.status(500).json(err);
       }
 });
+
+// delete user
+
+router.delete("/delete", authenticate, async (req, res) => {
+      try {
+        await User.deleteOne({ email: req.user.email});
+        res.status(200).send("Account has been deleted");
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+  });
 
 module.exports = router;
