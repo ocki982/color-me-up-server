3d198eb695cea8dae9bbd3d0cdfde817fc275e00
ocki982 <maiderocta@gmail.com>
2022-05-19 20:27:51 -0700

    created delete and like route on posts

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-    userId: {
+    user: {
         type: String,
         require: true,
       },
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router(); 
 const Post = require("../models/Post");
+const authenticate = require('../middleware/authenticate');
+
+// post a new comment
 
 router.post("/", (req, res) => {
     const newPost = new Post(req.body);
@@ -11,5 +14,33 @@ router.post("/", (req, res) => {
       res.status(500).json(err);
     }
   });
+
+
+// delete a post if user is logged in
+
+router.delete("/delete", authenticate, async (req, res) => {
+    try {
+      await Post.deleteOne({ user: req.user.username});
+      res.status(200).send("Post has been deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+});
+
+// like a comment
+
+router.put("/:id/like", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (!post.likes.includes(req.body.user)) {
+        await post.updateOne({ $push: { likes: req.body.user } });
+        res.status(200).json("The post has been liked");
+      } 
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+// get all comments
 
 module.exports = router;
